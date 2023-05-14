6e0715b546fb83cd6f46fddf68abbe78e9d05ee0
n.khosravi <khosravi.neda@gmail.com>
2023-05-14 19:18:39 +0330

    Add gitignore and controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,19 +1,27 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+  constructor() {
+    // DONT DO THIS ON REAL APP
+    // USE DEPENDENCY INJECTION
+    this.messagesService = new MessagesService();
+  }
   @Get()
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
